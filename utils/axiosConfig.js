eafbf48ed64afe40456a13cc5094fde3eb284920
JEANAWAH <78942246+JEANAWAH@users.noreply.github.com>
2025-10-01 08:08:14 +0100

    Update axiosConfig.js

--- utils/axiosConfig.js
+++ utils/axiosConfig.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const RAW_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000";
+  // process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://tidylinker.com";
 const BASE_URL = RAW_BASE_URL.replace(/\/+$/, ""); // strip trailing slash
 
 let isRefreshing = false;
