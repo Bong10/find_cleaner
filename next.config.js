dc28ccfc830a67ba848fd50c5a27eac2701986ee
Bong10 <110461400+Bong10@users.noreply.github.com>
2026-01-05 15:44:13 -0500

    Change API base URL to HTTPS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 
-const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://217.154.36.63/";
+const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "https://cgsabiozard.co.uk";
 const { protocol, hostname, port } = new URL(apiUrl);
 
 /** @type {import('next').NextConfig} */
