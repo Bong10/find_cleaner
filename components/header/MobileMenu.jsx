f39a60fb6bd1ac5d5f63d294d84319869ab27871
Bong10 <110461400+Bong10@users.noreply.github.com>
2026-06-22 19:26:41 -0400

    Update Link href in MobileMenu component

--- components/header/MobileMenu.jsx
+++ components/header/MobileMenu.jsx
@@ -47,9 +47,10 @@ const MobileMenu = () => {
             <div className="nav-outer">
               <div className="logo-box">
                 <div className="logo">
-                  <Link href={isAuthenticated ? getDashboardRoute() : "/"}
-                      style={{ position: "relative", zIndex: 1000, pointerEvents: "auto" }}
-                    >
+                  <Link
+                    href="/"
+                    style={{ position: "relative", zIndex: 1000, pointerEvents: "auto" }}
+                  >
                     <Image
                       width={40}
                       height={36}
